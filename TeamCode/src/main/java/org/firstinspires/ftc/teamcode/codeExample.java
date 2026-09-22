6b9d028a7959cdf930bd522ca3ed093b85b8d1f4
30stayale <30stayale@washk12.org>
2026-09-22 16:31:00 -0600

    fixed the frontLeft thing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/codeExample.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/codeExample.java
@@ -8,7 +8,7 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 
 @TeleOp
 
-public class CodeExample extends LinearOpMode {
+public class codeExample extends LinearOpMode {
     private DcMotor frontLeft;
     private DcMotor frontRight;
     private DcMotor backLeft;
@@ -17,9 +17,9 @@ public class CodeExample extends LinearOpMode {
     @Override
     public void runOpMode() {
         frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
-        frontLeft = hardwareMap.get(DcMotor.class, "frontRight");
-        frontLeft = hardwareMap.get(DcMotor.class, "backLeft");
-        frontLeft = hardwareMap.get(DcMotor.class, "backRight");
+        frontRight = hardwareMap.get(DcMotor.class, "frontRight");
+        backLeft = hardwareMap.get(DcMotor.class, "backLeft");
+        backRight = hardwareMap.get(DcMotor.class, "backRight");
 
         telemetry.addData("Status", "Initialized");
         telemetry.update();
@@ -29,7 +29,7 @@ public class CodeExample extends LinearOpMode {
         // run until the end of the match (driver presses STOP)
         double tgtPower = 0;
         while (opModeIsActive()) {
-            tgtPower = -this.gamepad1.left_stick_y;
+            tgtPower = -this.gamepad2.left_stick_y;
             frontLeft.setPower(tgtPower);
             telemetry.addData("Target Power", tgtPower);
             telemetry.addData("Motor Power", frontLeft.getPower());
