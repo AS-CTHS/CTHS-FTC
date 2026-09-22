88984322c35ec23df77569f769b9c26e0ddcc651
30stayale <30stayale@washk12.org>
2026-09-22 16:50:38 -0600

    TeleOP Initial commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.java
@@ -0,0 +1,38 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@com.qualcomm.robotcore.eventloop.opmode.TeleOp
+
+public class TeleOp extends LinearOpMode {
+    private DcMotor frontLeft;
+    private DcMotor frontRight;
+    private DcMotor backLeft;
+    private DcMotor backRight;
+
+    @Override
+    public void runOpMode() {
+        frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
+        frontRight = hardwareMap.get(DcMotor.class, "frontRight");
+        backLeft = hardwareMap.get(DcMotor.class, "backLeft");
+        backRight = hardwareMap.get(DcMotor.class, "backRight");
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+
+        // run until the end of the match (driver presses STOP)
+        double tgtPower = 0;
+        while (opModeIsActive()) {
+            tgtPower = -this.gamepad2.left_stick_y;
+            frontLeft.setPower(tgtPower);
+            telemetry.addData("Target Power", tgtPower);
+            telemetry.addData("Motor Power", frontLeft.getPower());
+            telemetry.addData("Status", "Running");
+            telemetry.update();
+
+        }
+    }
+}
